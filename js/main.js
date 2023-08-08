377f91e2db85541efc67382f1b04b10325ce82c0
vitkenji <vkz.yamada@gmail.com>
2023-08-08 16:59:23 -0300

    files added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+
+})
